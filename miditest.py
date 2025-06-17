719601e4a881c1184b91f44375af0832ae8f368d
gespel <heimbrodt@uni-potsdam.de>
2025-06-17 21:13:09 +0200

    found bug

--- miditest.py
+++ miditest.py
@@ -1,6 +1,7 @@
 import pygame.midi
 import time
-import librosa
+#import librosa
+import random
 
 def list_midi_devices():
     pygame.midi.init()
@@ -18,20 +19,15 @@ def list_midi_devices():
 def send_midi_sequence(device_id):
     player = pygame.midi.Output(device_id)
     print(f"Sende Sequenz an Gerät {device_id}")
-    notes = [note_to_midi_nr("c3"), note_to_midi_nr("d3"), note_to_midi_nr("c#3")]
+    notes = [58, 60, 62, 61, 70, 75]
     print(notes)
 
+    print("Sending")
     while True:
-        print("Sending")
         for note in notes:
-            player.note_on(note, velocity=100)
+            player.note_on(notes[random.randint(0, len(notes)-1)], velocity=100)
             time.sleep(0.1)
             player.note_off(note, velocity=100)
-    player.close()
-
-def note_to_midi_nr(note):
-    return int(librosa.note_to_midi(note))
-
     
 
 if __name__ == "__main__":
